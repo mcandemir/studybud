dd561eeb146f037973b25020f95aaf0e99a6677a
Can Demir <mhmtcndmr01@gmail.com>
2024-01-11 22:55:00 +0300

    finilizing

---
+++ README.md
@@ -0,0 +1,39 @@
+# StudyBud
+
+StudyBud is a website where you can create chat rooms about your favorite topic, browse other topics and talk with other people about it!
+
+
+### Login or Sign Up:
+![Alt text](previewimg/image-2.png)
+
+
+### Main page:
+![Alt text](previewimg/image-1.png)
+
+
+### Create rooms:
+![Alt text](previewimg/image-4.png)
+
+
+### Participate:
+![Alt text](previewimg/image-3.png)
+
+
+### Customize your profile:
+![Alt text](previewimg/image-5.png)
+
+
+### Get the code
+```shell
+$ git clone https://github.com/mcandemir/studybud.git
+```
+
+### Default Profiles in Database:
+    username mail: can@email.com
+    password: Django1230
+
+    username mail: tim@email.com
+    password: Django1230
+
+    username mail: eric@email.com
+    password: Django1230
--- base/models.py
+++ base/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    pass
 
 
 class Topic(models.Model):
@@ -35,6 +46,11 @@ class Message(models.Model):
     body = models.TextField()
     updated = models.DateField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # `update` => ascending order
+        # `-update` => descending order (newest)
+        ordering = ['-updated', '-created']
 
     def __str__(self) -> str:
         return self.body[0:50]
